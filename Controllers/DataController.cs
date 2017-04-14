9d3c5ed78030b1712f6fa8ecbadd5cb9ba62e583
spubissmart <dzulawski@gmail.com>
2017-04-14 17:16:52 +0900

    Add files via upload

---
+++ Controllers/DataController.cs
@@ -0,0 +1,37 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Newtonsoft.Json;
+using ActUtlTypeLib;
+using System.Web.UI.HtmlControls;
+
+namespace PLCReadApp.Controllers
+{
+    public class PLCReadController : ApiController
+    {
+        plcData plcData = new plcData();
+
+        [HttpGet]
+        public int[] readPLC(string address, int len, int station)
+        {
+            var plc = new ActUtlType();
+
+            var plcaddr = address;
+            plc.ActLogicalStationNumber = station;
+
+            var iReturnCode = plc.Open();
+
+            var data = new int[960];
+
+            plc.ReadDeviceBlock(address, 960, out data[0]);
+
+            plc.Close();
+
+            return data;
+
+        }
+    }
+}
---
+++ Models/plcPoll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace ProductsApp.Models
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class plcData
+    {
+        [JsonProperty("address")]
+        public string Address { get; set; }
+        [JsonProperty("len")]
+        public int len { get; set; }
+
+        static void Main(string[] args)
+        {
+
+        }
+
+
+
+
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
